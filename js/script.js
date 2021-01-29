983ace52abf7670bf34f6592fd34b1bfb81da1eb
Jonah Johansen <johansenjonah+git@gmail.com>
2021-01-29 13:58:52 -0800

    add cart total and individual item cost calculation

--- js/script.js
+++ js/script.js
@@ -33,8 +33,18 @@ function Pizza() {
   this.cost = 0;
 }
 
-Pizza.calcprice = function() {
-
+Pizza.prototype.calcprice = function(priceBook) {
+  price = 0;
+  price += parseInt(priceBook.sizes[this.size]);
+  price += parseInt(priceBook.crusts[this.crusts]);
+  price += parseInt(priceBook.cFlavor[this.cFlavor]);
+  price += parseInt(priceBook.sauce[this.sauce]);
+  price += parseInt(priceBook.cheese[this.cheese]);
+  price += parseInt(priceBook.specials[this.specials]);
+  this.toppings.forEach(top => {
+    price += parseInt(priceBook.toppings[top])
+  })
+  this.cost = price;
 }
 
 Pizza.prototype.add = function(type,stuff) {
@@ -44,11 +54,11 @@ Pizza.prototype.add = function(type,stuff) {
 
 const PriceBook = {
   sizes : {party:"50",large:"30",medium:"20",personal:"10",single:"4"},
-  crusts: {handtoss:"3",thin:"2",stuff:"4",orig:"2"},
-  cFlavor: {garlic:"3",toast:"3",cheese:"5"},
-  sauce : {mar:"2",garlic:"1",barbaque:"3",buffalo:"1"},
+  crusts: {handtoss:"3",thin:"2",stuffed:"4",orig:"2"},
+  cFlavor: {garlicButtery:"3",toast:"3",cheese:"5"},
+  sauce : {mar:"2",garlicParmesan:"1",barbaque:"3",buffalo:"1"},
   cheese: {light:"0",regular:"2",extra:"5"},
-  toppings: {meats: {pepperoni:"4",italian:"5",meatball:"4",ham:"6",bacon:"6",chicken:"3",beef:"4",pork:"4",mushrooms:"2",onions:"1",olives:"2",bellPeppers:"2",bananaPeppers:"3",pineapple:"4",jalapeno:"3",tomato:"2",spinach:"1"}},
+  toppings: {pepperoni:"4",italian:"5",meatball:"4",ham:"6",bacon:"6",chicken:"3",beef:"4",pork:"4",mushrooms:"2",onions:"1",olives:"2",bellPeppers:"2",bananaPeppers:"3",pineapple:"4",jalapeno:"3",tomato:"2",spinach:"1"},
   specials: {anchovies:"8",will:"4"}
 }
 
@@ -81,27 +91,32 @@ $(document).ready(function(){
     $("#cart>ul").html("");
     for (i=0;i<cart.items.length;i++) {
       console.log(cart.items[i]);
-      $("#cart>ul").append(`<li class="${i}"><h3>${cart.items[i].crusts} ${cart.items[i].size} Pizza</h3></li>`)
+      $("#cart>ul").append(`<li class="${i}"><h3>${cart.items[i].crusts} ${cart.items[i].size} Pizza  Cost:${cart.items[i].cost}</h3></li>`)
     }
+    cart.getTotal();
+    $("#carttotal").text(cart.total)
     $("#cart").show();
   });
 
   $("#closeCart").on("click", function() {
     $("#cart").hide();
   });
-
-
   
   $("#makepizza").on("click", function() {
+    controlCurrent = 0;
     $(".pizzamaker").show();
     cart.setActive(new Pizza())
     //set that pizza as active object to modify
   });
 
   $("#addpizza").on("click", function() {
-    $(".pizzamaker").hide();
-    cart.addToOrder(cart.active)
-    cart.setActive({})
+    if (cart.active.size == "" || cart.active.crusts == "" || cart.active.cFlavor == "" || cart.active.sauce == "" || cart.active.cheese == "" || cart.active.toppings.length == 0) alert("Completely fill out pizza before submitting")
+    else {
+      $(".pizzamaker").hide();
+      cart.active.calcprice(PriceBook);
+      cart.addToOrder(cart.active);
+      cart.setActive({});
+    }
   });
 
   $("#control>div>ul>li").on("click", function() {
